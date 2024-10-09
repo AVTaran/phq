befb2d328e109d6db8c5f2e8bd96be73f2a453e3
Aleksandr Taran <aleks.t.dev@gmail.com>
2024-10-09 14:14:16 +1300

    Search component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { ListService } from '../services/list.service';
 import { CommonModule } from '@angular/common';
 import { LinkModule } from '../model/link/link.module';
+import { ActivatedRoute, Router } from '@angular/router';
+import {SearchComponent} from '../search/search.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [CommonModule, SearchComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -15,10 +17,17 @@ export class HomeComponent {
 
   listLinks: LinkModule[] = [];
 
-  constructor(private listService: ListService) { }
+  constructor(private listService: ListService, private router: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listLinks = this.listService.getAll();
+    this.router.params.subscribe(params => {
+      if(params['query']) {
+        this.listLinks = this.listService.getAll()
+          .filter(list => list.name.toLowerCase().includes(params['query'].toLowerCase()));
+      } else {
+        this.listLinks = this.listService.getAll();
+      }
+    })
   }
 
 }
